e6664e59750d2de0dc0e5e343ca20825294abf78
Jyoti Shah <jyotishah@Jyotis-MacBook-Air.local>
2026-02-01 19:01:32 +0545

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,15 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  esbuild: {
-    loader: 'jsx',
-    include: /src\/.*\.jsx?$/,
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      loader: {
-        '.js': 'jsx',
-      },
-    },
-  },
 })
